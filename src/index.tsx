0f2a4ea0e59a227a881f47f95e36d7f0411376cc
richard-marc <richardsookhai21@gmail.com>
2024-09-10 16:49:04 -0600

    abababbaba

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Helmet } from 'react-helmet';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Helmet>
+      <title>rikko.cool</title>
+    </Helmet>
     <App />
   </React.StrictMode>
 );
